5b4e6da4089c5ace1dfad16ab64a1e0d21114c24
PuneethKarkera <impuneeth.pk@gmail.com>
2018-06-07 23:06:53 +0530

    Task scheduling app

---
+++ app/src/main/java/nocompany/notifier/AlarmReciever.java
@@ -0,0 +1,28 @@
+package nocompany.notifier;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AlarmReciever extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        int notificationId = intent.getIntExtra("notificationId", 0);
+        String message = intent.getStringExtra("todo");
+
+        Intent mainIntent = new Intent(context, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, mainIntent, 0);
+
+        NotificationManager myNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Notification.Builder builder = new Notification.Builder(context);
+        builder.setSmallIcon(android.R.drawable.ic_dialog_info).setContentTitle("Reminder!!!").setContentText(message)
+                .setWhen(System.currentTimeMillis()).setAutoCancel(true).setContentIntent(contentIntent);
+
+        myNotificationManager.notify(notificationId, builder.build());
+    }
+}
